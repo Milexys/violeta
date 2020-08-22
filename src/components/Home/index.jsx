2b0b94ccb59c282db1f7ef60df0260c3aa702899
milexys96 <milexysferrer@gmail.com>
2020-08-22 14:04:10 -0400

    proyecto

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,6 +6,10 @@ import karenQ from '../../images/karenQ.jpg'
 import karenR from '../../images/karenR.jpg'
 import mile from '../../images/milexys.jpg'
 
+import presentacion from '../../images/presentacion.jpeg'
+import mobile from '../../images/mobile.jpeg'
+import figma from '../../images/figma.png'
+
 import Fade from 'react-reveal/Fade';
 
 const Home = () => {
@@ -41,27 +45,42 @@ const Home = () => {
                     </div>
                 </Fade>
             </div>
-
-            <div>
-
             <div className="items">
-                            <div>
-                                <img src={karenQ} alt="logo" />
-                                <p>Karen Quezada</p>
-                            </div>
-                            <div>
-                                <img src={karenR} alt="logo" />
-                                <p>Karen Reyes</p>
-                            </div>
-                            <div>
-                                <img src={mile} alt="logo" />
-                                <p>Milexys Ferrer</p>
-                            </div>
-                            <div>
-                                <img src={sara} alt="logo" />
-                                <p>Sarays Guarguana</p>
-                            </div>
+                <div className="contenedor">
+                    <div className="card">
+                        <div className="img">
+                            <img src={presentacion} alt="logo" />
                         </div>
+                        <div className="texto">
+                            <h4>Research Study</h4>
+                            <p>Estudio realizado sobre el proyecto</p>
+                            <a href="https://docs.google.com/presentation/d/16WvAp9HeWL6NX0aDEjvqwYu7WApTSjx6juFz-G_4RCQ/edit#slide=id.gc6f80d1ff_0_27" target="_blank">Ver</a>
+                        </div>
+                    </div>
+                    <div className="card">
+
+                        <div className="img">
+                            <img src={figma} alt="logo" />
+                        </div>
+                        <div className="texto">
+                            <h4>Figma</h4>
+                            <p>Prototipo de alta fidelidad realizado en Figma, versión mobile.</p>
+                            <a href="https://www.figma.com/file/8Pkj9sZhr3UYZvxzJRzndl/Violeta?node-id=0%3A1" target="_blank">Ver</a>
+                        </div>
+                    </div>
+                    <div className="card">
+
+                        <div className="img">
+                            <img src={mobile} alt="logo" />
+                        </div>
+                        <div className="texto">
+                            <h4>Prototipo de Alta Fidelidad</h4>
+                            <p>Prototipo de alta fidelidad realizado en Figma, versión mobile.</p>
+                            <a href="https://www.figma.com/proto/8Pkj9sZhr3UYZvxzJRzndl/Violeta?node-id=89%3A124&scaling=scale-down" target="_blank">Ver</a>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </Container>
     );
--- src/components/Home/style.css.jsx
+++ src/components/Home/style.css.jsx
@@ -9,6 +9,10 @@ export const Container = styled.main`
     display: grid;
     grid-template-columns: repeat(2, 50vw);
     grid-template-rows: 2fr;
+    @media (max-width: 760px){
+        grid-template-columns: 1fr;
+        grid-template-rows: repeat(2, 100vh);
+    }
     .container{
         grid-column: 1/2;
         grid-row: 1/3;
@@ -16,6 +20,9 @@ export const Container = styled.main`
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         padding: 2rem 1.5rem;
         display: grid;
+        @media (max-width: 760px){
+            grid-row: 1/2;
+    }
     }
     .descripcion{
         margin: auto;
@@ -48,9 +55,57 @@ export const Container = styled.main`
     .items{
         grid-column: 2/3;
         grid-row: 1/3;
-        img{
-            width: 100px;
-            height: 100px;
+        display: grid;
+        background:#ffeafc;
+        @media (max-width: 760px){
+            grid-column: 1/2;
+            grid-row: 2/3;
+    }
+        .contenedor{
+            width: 70%;
+            margin: auto;
+            display: grid;
+            grid-template-rows: 4fr;
+            grid-gap: 3px;
+                .card{
+                border: 2px solid ${theme.morado};
+                border-radius: 10px;
+                margin: 10px;
+                display: flex;
+                padding: 3px 1px;
+                .texto{
+                    width: 100%;
+                    a{
+                        text-decoration: none;
+                        color: #ffffff;
+                        background-color: ${theme.morado};
+                        padding: 2px 1.5rem;
+                        border-radius: 10px;
+                        margin: 0 auto 10px auto;
+                        display: block;
+                        width: 50px;
+                    }
+                    p{
+                        margin: 8px 0 14px 0;
+                        font-size: 14px;
+                    }
+                    h4{
+                        margin: 0;
+                    }
+                }
+                .img{
+                    max-height: 100px;
+                    width: 150px;
+                   
+                }
+                    img{
+                    width: auto;
+                    max-height: 100%;
+                    border-radius: 10px;
+                }
+            }
         }
+  
+     
     }
 `
